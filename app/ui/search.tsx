19b556811c1336c9cddd380f5ce87fff2fbcd3bd
shady-abdelhamid <shady.abdelhamid@outlook.com>
2025-03-30 12:18:46 +0200

    Refactor search component to improve query handling and utilize pathname for URL updates

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,22 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
 
   const handleSearch = (term: string) => {
     const params = new URLSearchParams(searchParams);
-    console.log({ term });
+
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathName}?${params.toString()}`);
   };
 
   return (
@@ -19,7 +27,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e)=> handleSearch(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
