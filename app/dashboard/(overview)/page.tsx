3bb128d4d29aaf25113f04df4dddcb62319f1782
shady-abdelhamid <shady.abdelhamid@outlook.com>
2025-03-30 11:17:13 +0200

    Refactor dashboard cards to use CardWrapper component and implement loading state with skeleton UI

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,16 @@
 import { fetchCardData } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
-import { Card } from "../../ui/dashboard/cards";
+import CardWrapper from "../../ui/dashboard/cards";
 import { default as LatestInvoices } from "../../ui/dashboard/latest-invoices";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../../ui/skeletons";
+import {
+  CardSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "../../ui/skeletons";
 
 export default async function Page() {
-
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -21,14 +24,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
